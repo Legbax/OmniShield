954b5b59ddf50db992ec7dbb35d40f51c71d4a81
Legbax <papalegba.services@gmail.com>
2026-03-03 02:36:16 -0300

    Revert "feat: Implement hybrid spoofing for cosmetic properties via resetprop daemon"
    This reverts commit 28307fa9bf074398cd25c5b33de5f8b138381e98.

--- jni/main.cpp
+++ jni/main.cpp
@@ -5459,10 +5459,10 @@ public:
                                 "mPermissionsControllerPackageName", "Ljava/lang/String;");
                             if (env2->ExceptionCheck()) env2->ExceptionClear();
                             if (field) {
-                                jstring aosp = env2->NewStringUTF("com.android.permissioncontroller");
+                                jstring aosp = env2->NewStringUTF("com.google.android.permissioncontroller");
                                 env2->SetObjectField(pm, field, aosp);
                                 LOGE("MIUI fix: forced mPermissionsControllerPackageName -> "
-                                     "com.android.permissioncontroller");
+                                     "com.google.android.permissioncontroller");
                             }
                             env2->DeleteLocalRef(pm);
                         }
